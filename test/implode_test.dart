3531fae93c6037f1c510816071f7ae317a45c8a7
viiviii <viiiix.viiviii@gmail.com>
2022-03-17 02:17:16 +0900

    :white_check_mark: 테스트 예시 및 설명 수정

--- test/implode_test.dart
+++ test/implode_test.dart
@@ -11,45 +11,40 @@ main() {
       ['ㅂㅜㄹㄷㅏㄹㄱㅇㅡㄴ', '불닭은'],
       ['ㅇㅓㅂㅔㄴㅈㅕㅅㅡ ㅇㅐㄴㄷㅡㄱㅔㅇㅣㅁ', '어벤져스 앤드게임'],
     ].forEach((e) {
-      final hints = e[0];
-      final text = e[1];
+      final hints = e.first;
+      final text = e.last;
       test('implode $hints → $text', () {
         expect(implode(hints), text);
       });
     });
   });
 
-  group('mixedConsonantLetters', () {
-    test('인접한 모음을 복합 모음으로 합친다', () {
-      expect(mixedConsonantLetters(['ㅇ', 'ㅜ', 'ㅓ', 'ㄴ']), ['ㅇ', 'ㅝ', 'ㄴ']);
+  group('mixMedial', () {
+    test('ㅗㅏ -> ㅘ', () {
+      expect(mixMedial(['ㅇ', 'ㅗ', 'ㅏ']), ['ㅇ', 'ㅘ']);
     });
     test('합칠 수 있는 복합 모음이 없는 경우 변경 사항이 없다', () {
-      expect(mixedConsonantLetters(['ㅇ', 'ㅣ', 'ㅇ']), ['ㅇ', 'ㅣ', 'ㅇ']);
-      expect(mixedConsonantLetters(['ㅇ', 'ㅣ', 'ㅜ']), ['ㅇ', 'ㅣ', 'ㅜ']);
+      expect(mixMedial(['ㅇ', 'ㅣ', 'ㅇ']), ['ㅇ', 'ㅣ', 'ㅇ']);
+      expect(mixMedial(['ㅇ', 'ㅣ', 'ㅜ']), ['ㅇ', 'ㅣ', 'ㅜ']);
     });
   });
 
-  // TODO(viiviii): 이최선인가?
-  group('makeGroupsUsingVowelLetters', () {
-    test('모음을 기준으로 Group 리스트를 만든다', () {
-      final groups =
-          makeGroupsUsingVowelLetters(['ㅂ', 'ㅜ', 'ㄹ', 'ㄷ', 'ㅏ', 'ㄱ']);
+  group('createGroupsByMedial', () {
+    test('ㄱ, ㅡ , ㄹ, ㅐ -> [ㄱ], [ㅡ, ㄹ], [ㅐ]', () {
+      final groups = createGroupsByMedial(['ㄱ', 'ㅡ', 'ㄹ', 'ㅐ']);
+
       final group1 = groups[0];
-      expect(group1.initials, isEmpty);
-      expect(group1.medial, '');
-      expect(group1.finales, ['ㅂ']);
+      expect(group1.finales, ['ㄱ']);
       final group2 = groups[1];
-      expect(group2.initials, isEmpty);
-      expect(group2.medial, 'ㅜ');
-      expect(group2.finales, ['ㄹ', 'ㄷ']);
+      expect(group2.medial, 'ㅡ');
+      expect(group2.finales, ['ㄹ']);
       final group3 = groups[2];
-      expect(group3.initials, isEmpty);
-      expect(group3.medial, 'ㅏ');
-      expect(group3.finales, ['ㄱ']);
+      expect(group3.medial, 'ㅐ');
     });
   });
 
-  group('replaceTheRemainingFinalesToInitials', () {
+  // TODO(viiviii)
+  group('mixFinaleAndReplaceTheRemainingFinalesToInitials', () {
     test('앞 그룹에 중성이 없는 경우', () {
       //given
       const previousFinales = ['ㄱ', 'ㄱ'];
@@ -58,8 +53,8 @@ main() {
       final current = Group.from(medial: 'ㅏ');
 
       //when
-      final groups = mixFinalesAndReplaceTheRemainingFinalesToInitials(
-          [previous, current]);
+      final groups =
+          mixFinaleAndReplaceTheRemainingFinalesToInitials([previous, current]);
 
       //then
       final previousActual = groups.first;
@@ -76,8 +71,8 @@ main() {
       final current = Group.from(medial: 'ㅏ');
 
       //when
-      final groups = mixFinalesAndReplaceTheRemainingFinalesToInitials(
-          [previous, current]);
+      final groups =
+          mixFinaleAndReplaceTheRemainingFinalesToInitials([previous, current]);
 
       //then
       final previousActual = groups.first;
@@ -97,7 +92,7 @@ main() {
 
     //when
     final groups =
-        mixFinalesAndReplaceTheRemainingFinalesToInitials([previous, current]);
+        mixFinaleAndReplaceTheRemainingFinalesToInitials([previous, current]);
 
     //then
     final previousActual = groups.first;
@@ -106,7 +101,6 @@ main() {
     expect(currentActual.initials, previousFinales);
   });
 
-  // TODO(viiviii): 이 케이스를 제외하고는 모두 같다 -> 해당 메서드 분기 수정
   test('앞 그룹에 중성이 있고, 종성이 여러개인 경우', () {
     //given
     const previousMedial = 'ㅗ';
@@ -117,7 +111,7 @@ main() {
 
     //when
     final groups =
-        mixFinalesAndReplaceTheRemainingFinalesToInitials([previous, current]);
+        mixFinaleAndReplaceTheRemainingFinalesToInitials([previous, current]);
 
     //then
     final previousActual = groups.first;
@@ -128,7 +122,7 @@ main() {
   });
 
   group('divideByHangulBlock', () {
-    test('[[ㄲ], ㅗ, [ㅊ]] -> [ㄲ, ㅗ, ㅊ]', () {
+    test('[ㄲ], ㅗ, [ㅊ] -> [ㄲ, ㅗ, ㅊ]', () {
       final group = Group.of(['ㄲ'], 'ㅗ', ['ㅊ']);
       expect(divideByHangulBlock([group]), [
         ['ㄲ', 'ㅗ', 'ㅊ']
